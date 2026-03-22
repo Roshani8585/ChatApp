7bb4f7c58c5d497ed8441046f479230707ca3aa7
Roshani Yadav <roshaniydv2001@gmail.com>
2026-03-22 15:55:27 +0530

    docker added

--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ builder.Services.AddTransient<EmailService>();
 
 var app = builder.Build();
 
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
 app.UseStaticFiles();
 app.UseRouting();
 
